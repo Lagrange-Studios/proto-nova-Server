fa7ca9269319171832debb9c5de99ac74e93e3d3
Frost <sniper321kc@gmail.com>
2025-12-28 10:59:45 -0600

    Updated Client Prediciton

--- src/main/java/socket/PacketReciver.java
+++ src/main/java/socket/PacketReciver.java
@@ -13,8 +13,7 @@ public class PacketReciver {
 	private EntityFinder entityFinder;
 	private ChunkManager chunkManager;
 	private EntityManager entityManager;
-	private final int  TPS = 60;
-	private final double reconcileDistance = 0.5; // nessecary distance to reconcile
+	private final double reconcileDistance = 0.1; // nessecary distance to reconcile
 	
 	public PacketReciver(EntityFinder entityFinder, ChunkManager chunkManager, EntityManager entityManager) {
 		this.entityFinder = entityFinder;
@@ -25,9 +24,14 @@ public class PacketReciver {
 	public void recivePacket(Player player, ClientToServerPacket packet) {
 		
 		// simulate the entity serverside
+		
+		// simulate keyPresses
 		for (int i=0;i<packet.getActionsCount();i++) {
-			entityManager.updateEntity(EntitySimulation.simulateMovement(entityManager.getEntity(player), TPS, packet.getActions(i)));
+			entityManager.updateEntity(EntitySimulation.simulateMovement(entityManager.getEntity(player), packet.getActions(i)));
 		}
+		
+		// Simulate final velocity
+		entityManager.updateEntity(EntitySimulation.simulateVelocity(entityManager.getEntity(player)));
 		
 		if (VectorMath.distance(packet.getUpdatedEntity().getPosition(), entityManager.getEntity(player).getPosition()) >= reconcileDistance) {
 			player.shouldReconcile = true;
